c69f1e27200a570aea20e953b3ec7f1b6db4d7ee
Naveed Ullah <naveed.dev30@gmail.com>
2023-06-16 15:00:34 +0500

    Update

--- lib/init/init.dart
+++ lib/init/init.dart
@@ -2,7 +2,8 @@ import 'dart:io';
 import 'package:path/path.dart' as path;
 
 init() async {
-  String packageDirectory = path.dirname(Platform.script.toFilePath());
+  String packageDirectory = path.dirname(Platform.resolvedExecutable);
+
   String assetsPath = path.join(packageDirectory, 'lib');
   print(assetsPath);
 
