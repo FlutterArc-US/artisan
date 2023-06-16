3c4f148a3a03807f99b6e5ba6c896eccdf8ed29f
Naveed Ullah <naveed.dev30@gmail.com>
2023-06-16 14:16:23 +0500

    Update

--- lib/init/init.dart
+++ lib/init/init.dart
@@ -1,30 +1,6 @@
 import 'dart:io';
 import 'package:path/path.dart' as path;
 
-void copyDirectory(Directory sourceDir, Directory destinationDir) {
-  // Create the destination directory if it doesn't exist
-  if (!destinationDir.existsSync()) {
-    destinationDir.createSync(recursive: true);
-  }
-
-  // List all files and directories in the source directory
-  sourceDir.listSync().forEach((FileSystemEntity entity) {
-    if (entity is File) {
-      // If it's a file, copy it to the destination directory
-      File file = File(entity.path);
-      String relativePath = file.path.replaceFirst(sourceDir.path, '');
-      String destinationPath = destinationDir.path + relativePath;
-      file.copySync(destinationPath);
-    } else if (entity is Directory) {
-      // If it's a directory, recursively copy it
-      Directory subDirectory = Directory(entity.path);
-      String relativePath = subDirectory.path.replaceFirst(sourceDir.path, '');
-      String destinationPath = destinationDir.path + relativePath;
-      copyDirectory(subDirectory, Directory(destinationPath));
-    }
-  });
-}
-
 void init() {
   try {
     final packageDirectory = path.dirname(Platform.script.toFilePath());
@@ -35,8 +11,29 @@ void init() {
 
     print(toPath);
 
+    if (!toPath.existsSync()) {
+      toPath.createSync(recursive: true);
+    }
+
     copyDirectory(Directory(fromPath), toPath);
   } catch (e) {
     print(e);
   }
+}
+
+void copyDirectory(Directory source, Directory destination) {
+  source.listSync(recursive: true).forEach((entity) {
+    if (entity is File) {
+      final relativePath = path.relative(entity.path, from: source.path);
+      final destinationFile = path.join(destination.path, relativePath);
+      final destinationDirectory = Directory(path.dirname(destinationFile));
+
+      if (!destinationDirectory.existsSync()) {
+        destinationDirectory.createSync(recursive: true);
+      }
+
+      entity.copySync(destinationFile);
+      print('Copied file: $destinationFile');
+    }
+  });
 }
