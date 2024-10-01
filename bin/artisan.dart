cea45a40f08e0fc80d74911fda76f16102f31ebc
Muhammad Umer Malik <77432523+imumermalik@users.noreply.github.com>
2024-10-01 23:26:39 +0500

    Create View cmd  Added

--- bin/artisan.dart
+++ bin/artisan.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:artisan/functions/add_github_workflow.dart';
 import 'package:artisan/functions/create_feature.dart';
+import 'package:artisan/functions/create_view.dart'; // Import the createView function
 import 'package:artisan/init/init.dart';
 
 void main(List<String> args) {
@@ -26,6 +27,14 @@ void main(List<String> args) {
 
     case 'create:workflow':
       createGitHubWorkflow();
+      break;
+
+    case 'create:view':
+      if (args.length > 2) {
+        createView(args[1], args[2]); // Pass the view name and feature name
+      } else {
+        log("Please provide a view name and feature name.");
+      }
       break;
 
     default:
