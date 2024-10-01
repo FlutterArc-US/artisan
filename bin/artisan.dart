9c2c2840962429bbd0f45bb15170d1e699944111
Muhammad Umer Malik <77432523+imumermalik@users.noreply.github.com>
2024-10-01 19:17:02 +0500

    GitHub WorkFlow File Added

--- bin/artisan.dart
+++ bin/artisan.dart
@@ -1,21 +1,25 @@
 import 'dart:io';
 
+import 'package:artisan/files/main.yaml.dart';
+
 void main(List<String> args) {
   if (args.isEmpty) {
-    print("No command provided. Use 'artisan make:feature <feature_name>' to create a feature.");
+    print("No command provided.");
     return;
   }
 
   final command = args[0];
-  final featureName = args.length > 1 ? args[1] : null;
-
   switch (command) {
     case 'make:feature':
-      if (featureName != null) {
-        createFeature(featureName);
+      if (args.length > 1) {
+        createFeature(args[1]);
       } else {
         print("Please provide a feature name.");
       }
+      break;
+
+    case 'make:workflow':
+      createGitHubWorkflow();
       break;
 
     default:
@@ -27,19 +31,13 @@ void main(List<String> args) {
 void createFeature(String featureName) {
   print("Creating feature: $featureName");
 
-  // Create the feature directory
   final featureDir = Directory('lib/features/$featureName');
   if (!featureDir.existsSync()) {
     featureDir.createSync(recursive: true);
     print('Feature folder created at lib/features/$featureName');
 
-    // Create Data Layer with subfolders
     createDataLayer(featureName);
-
-    // Create Domain Layer with subfolders
     createDomainLayer(featureName);
-
-    // Create Presentation Layer with subfolders
     createPresentationLayer(featureName);
 
     print('Feature $featureName created successfully.');
@@ -50,41 +48,46 @@ void createFeature(String featureName) {
 
 /// Creates the data layer structure
 void createDataLayer(String featureName) {
-  final dataDir = Directory('lib/features/$featureName/data');
-  dataDir.createSync(recursive: true);
-
-  // Create entities, repository, and source folders
+  Directory('lib/features/$featureName/data').createSync(recursive: true);
   Directory('lib/features/$featureName/data/entities').createSync(recursive: true);
   Directory('lib/features/$featureName/data/repository').createSync(recursive: true);
   Directory('lib/features/$featureName/data/source/local').createSync(recursive: true);
   Directory('lib/features/$featureName/data/source/remote').createSync(recursive: true);
 
-  print('Data layer structure created for $featureName');
+  print('Data layer created for $featureName');
 }
 
 /// Creates the domain layer structure
 void createDomainLayer(String featureName) {
-  final domainDir = Directory('lib/features/$featureName/domain');
-  domainDir.createSync(recursive: true);
-
-  // Create data, models, repository, and usecases folders
+  Directory('lib/features/$featureName/domain').createSync(recursive: true);
   Directory('lib/features/$featureName/domain/data').createSync(recursive: true);
   Directory('lib/features/$featureName/domain/models').createSync(recursive: true);
   Directory('lib/features/$featureName/domain/repository').createSync(recursive: true);
   Directory('lib/features/$featureName/domain/usecases').createSync(recursive: true);
 
-  print('Domain layer structure created for $featureName');
+  print('Domain layer created for $featureName');
 }
 
 /// Creates the presentation layer structure
 void createPresentationLayer(String featureName) {
-  final presentationDir = Directory('lib/features/$featureName/presentation');
-  presentationDir.createSync(recursive: true);
-
-  // Create providers, views, and widgets folders
+  Directory('lib/features/$featureName/presentation').createSync(recursive: true);
   Directory('lib/features/$featureName/presentation/providers').createSync(recursive: true);
   Directory('lib/features/$featureName/presentation/views').createSync(recursive: true);
   Directory('lib/features/$featureName/presentation/widgets').createSync(recursive: true);
 
-  print('Presentation layer structure created for $featureName');
+  print('Presentation layer created for $featureName');
+}
+
+/// Function to create the GitHub Workflow in the .github/workflows folder.
+void createGitHubWorkflow() {
+  gitHubWorklowFile;
+
+  final workflowDir = Directory('.github/workflows');
+  if (!workflowDir.existsSync()) {
+    workflowDir.createSync(recursive: true);
+    print('.github/workflows directory created.');
+  }
+  final workflowFile = File('.github/workflows/main.yaml');
+  workflowFile.writeAsStringSync(gitHubWorklowFile);
+  print('GitHub workflow file created at .github/workflows/main.yaml');
 }
---
+++ lib/files/main.yaml.dart
@@ -0,0 +1,65 @@
+const gitHubWorklowFile = '''
+name: Flutter_Tests
+
+on:
+  push:
+    branches: [main]
+
+jobs:
+  tests:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v3
+
+      - name: Set up Flutter
+        uses: subosito/flutter-action@v2
+        with:
+          channel: "stable"
+          flutter-version: "3.19.2"
+
+      - name: Install dependencies
+        run: flutter pub get
+
+      - name: Run build_runner
+        run: flutter pub run build_runner build --delete-conflicting-outputs
+
+      - name: Increment version number
+        id: increment_version
+        run: |
+          current_version=\$(grep 'version:' pubspec.yaml | cut -d ' ' -f 2)
+          version_code=\$(echo \$current_version | cut -d '+' -f 2)
+          version_name=\$(echo \$current_version | cut -d '+' -f 1)
+
+          IFS='.' read -ra VERSION_PARTS <<< "\$version_name"
+          patch=\${VERSION_PARTS[2]}
+          patch=\$((patch+1))
+
+          new_version="\${VERSION_PARTS[0]}.\${VERSION_PARTS[1]}.\$patch"
+          new_version_code=\$((version_code+1))
+
+          sed -i "s/version: .*/version: \$new_version+\$new_version_code/" pubspec.yaml
+
+          echo "New version: \$new_version+\$new_version_code"
+
+      - name: Commit version bump
+        run: |
+          git config --local user.email "actions@github.com"
+          git config --local user.name "GitHub Actions"
+          git add pubspec.yaml
+          git commit -m "ci: Bump version to \${{ steps.increment_version.outputs.new_version }}"
+          git push
+
+      - name: Build APK
+        run: flutter build apk --release
+
+      - name: Upload APK to Firebase App Distribution
+        uses: wzieba/Firebase-Distribution-Github-Action@v1
+        with:
+          appId: \${{ secrets.FIREBASE_APP_ID }}
+          serviceCredentialsFileContent: \${{ secrets.CREDENTIAL_FILE_CONTENT }}
+          groups: testers
+          file: build/app/outputs/flutter-apk/app-release.apk
+          
+''';
