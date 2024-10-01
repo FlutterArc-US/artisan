0cccc54b8181efee61a2617d3d9f3d17d18a732c
Muhammad Umer Malik <77432523+imumermalik@users.noreply.github.com>
2024-10-02 00:49:13 +0500

    Create View Check updated

--- lib/functions/create_view.dart
+++ lib/functions/create_view.dart
@@ -1,13 +1,46 @@
 import 'dart:io';
+
 import 'package:artisan/extensions/color_print_extension.dart';
 import 'package:artisan/functions/create_feature.dart';
 import 'package:artisan/functions/update_router_paths.dart';
 import 'package:artisan/functions/update_router_routes.dart';
 
+/// Function to create a new view file for a given feature
 Future<void> createView(String viewName, String featureName) async {
   try {
     // Determine the base path of the project dynamically
     final projectPath = Directory.current.path;
+
+    // Define the path for the router file
+    final routerFile = File('$projectPath/lib/util/router/router.dart');
+
+    // Define the path for the paths file
+    final pathsFile = File('$projectPath/lib/util/router/paths.dart');
+
+    // Check if the router and paths already exist
+    if (await routerFile.exists() && await pathsFile.exists()) {
+      final routerContent = await routerFile.readAsString();
+      final pathsContent = await pathsFile.readAsString();
+
+      // Check if the route already exists
+      if (routerContent
+          .contains('path: RoutePaths.${viewName.toLowerCase()}')) {
+        'Error: Route already exists for: ${viewName.capitalize()}View'
+            .printRed();
+        return;
+      }
+
+      // Check if the path already exists
+      if (pathsContent
+          .contains('static const String ${viewName.toLowerCase()} =')) {
+        'Error: Path already exists for: ${viewName.capitalize()}View'
+            .printRed();
+        return;
+      }
+    } else {
+      'Error: Router or paths file does not exist.'.printRed();
+      return;
+    }
 
     // Define the directory for the feature correctly
     final featureDirectory =
@@ -15,9 +48,8 @@ Future<void> createView(String viewName, String featureName) async {
 
     // Create the feature directory if it doesn't exist
     if (!await featureDirectory.exists()) {
-       createFeature(featureName);
-       'Feature not found, creating feature: $featureName'.printYellow();
-      'NewFeature created: $featureName'.printGreen();
+      createFeature(featureName);
+      'Feature not found, creating feature: $featureName'.printYellow();
     }
 
     // Define the path for the new view file
@@ -54,7 +86,7 @@ class ${viewName.capitalize()}View extends StatelessWidget {
     await File(viewFilePath).writeAsString(viewContent);
     'View file created successfully: $viewFilePath'.printGreen();
 
-    // Check and update router paths and routes
+    // Update router paths and routes
     final routerPathsResult = await updateRouterPaths(featureName, viewName);
     final routerResult = await updateRouterRoutes(featureName, viewName);
 
