27658e445a65e8a094f575bda18a9a273367679b
Muhammad Umer Malik <77432523+imumermalik@users.noreply.github.com>
2024-10-04 18:45:26 +0500

    cStls and cStfl View addedwith cmd

--- bin/artisan.dart
+++ bin/artisan.dart
@@ -1,7 +1,10 @@
 import 'dart:developer';
 
+import 'package:artisan/extensions/color_print_extension.dart';
 import 'package:artisan/functions/add_github_workflow.dart';
 import 'package:artisan/functions/features_functions/create_feature.dart';
+import 'package:artisan/functions/features_functions/presentation_functions/create_consumer_stful_view.dart';
+import 'package:artisan/functions/features_functions/presentation_functions/create_consumer_stless_view.dart';
 import 'package:artisan/functions/features_functions/presentation_functions/create_provider.dart';
 import 'package:artisan/functions/features_functions/presentation_functions/create_stful_view.dart';
 import 'package:artisan/functions/features_functions/presentation_functions/create_stless_view.dart'; // Import the createView function
@@ -32,21 +35,39 @@ void main(List<String> args) {
       createGitHubWorkflow();
       break;
 
-    ///[Create Page View]
+    /// [Create Page View]
     case 'create:view':
       if (args.length > 2) {
         final viewName = args[1];
         final featureName = args[2];
 
-        // Check for optional parameter for Stateful/Stateless
+        // Check for specific commands and execute appropriate function
         if (args.contains('--stful')) {
-          createStatefulView(viewName, featureName); // Create Stateful Widget
+          createStatefulView(viewName, featureName); // Creates Stateful Widget
+          'Stateful view created successfully for $viewName.'.printBoldGreen();
+        } else if (args.contains('--stless')) {
+          createStatelessView(
+              viewName, featureName); // Creates Stateless Widget
+          'Stateless view created successfully for $viewName.'.printBoldGreen();
+        } else if (args.contains('--cstful')) {
+          createConsumerStatefulView(
+              viewName, featureName); // Creates Consumer Stateful Widget
+          'Consumer stateful view created successfully for $viewName.'
+              .printBoldGreen();
+        } else if (args.contains('--cstless')) {
+          createConsumerStatelessView(
+              viewName, featureName); // Creates Consumer Stateless Widget
+          'Consumer stateless view created successfully for $viewName.'
+              .printBoldGreen();
         } else {
-          // Default case for Stateless widget or if --stls is passed
-          createStatelessView(viewName, featureName); // Create Stateless Widget
+          // Default to Stateless Widget if no option is provided
+          createStatelessView(viewName, featureName);
+          'Stateless view created successfully for $viewName (default).'
+              .printBoldGreen();
         }
       } else {
-        log("Please provide a view name and feature name.");
+        // Show error message if insufficient arguments are provided
+        'Error: Please provide both a view name and feature name.'.printRed();
       }
       break;
 
@@ -67,8 +88,16 @@ void main(List<String> args) {
       break;
 
     default:
-      log("Invalid Command: $command");
+      // Handle invalid commands and show available options
+      'Invalid command. Please use one of the following commands:'.printRed();
+      '''
+  dart run artisan create:view MyView MyFeature          (creates default stateless view)
+  dart run artisan create:view MyView MyFeature --stless (creates stateless view)
+  dart run artisan create:view MyView MyFeature --stful  (creates stateful view)
+  dart run artisan create:view MyView MyFeature --cstless (creates consumer stateless view)
+  dart run artisan create:view MyView MyFeature --cstful  (creates consumer stateful view)
+  '''
+          .printBoldGreen();
+      break;
   }
 }
-
-
