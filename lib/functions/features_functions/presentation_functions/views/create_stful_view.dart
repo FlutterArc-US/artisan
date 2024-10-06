ca305f0c3ae0954d833304fe2db003ae1dcb8e39
Muhammad Umer Malik <77432523+imumermalik@users.noreply.github.com>
2024-10-06 18:50:36 +0500

    FNP  Provider fixed

--- lib/functions/features_functions/presentation_functions/views/create_stful_view.dart
+++ lib/functions/features_functions/presentation_functions/views/create_stful_view.dart
@@ -15,11 +15,10 @@ Future<void> createStatefulView(String viewName, String featureName) async {
     final viewFilePath =
         '${featureDirectory.path}/${viewName.toSnakeCase()}_view.dart';
 
-    // 1. Check if the view file already exists
     if (await File(viewFilePath).exists()) {
       'Error: View file already exists:'.printBoldRed();
       viewFilePath.printClickablePath();
-      return; // Abort the operation if the view file exists
+      return;
     }
 
     if (!await featureDirectory.exists()) {
@@ -27,15 +26,13 @@ Future<void> createStatefulView(String viewName, String featureName) async {
       'Feature not found, creating feature: $featureName'.printBoldOrange();
     }
 
-    // 3. Check if the route already exists before creating the view
     final routeExists = await doesRouteExist(featureName, viewName);
     if (routeExists) {
       'Error: Route for view $viewName already exists in $featureName'
           .printBoldRed();
-      return; // Abort the operation if the route already exists
+      return;
     }
 
-    // 4. If all checks pass, proceed to create the view file
     final viewContent = '''
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -62,16 +59,13 @@ class _${viewName.toPascalCase()}ViewState extends State<${viewName.toPascalCase
 }
 ''';
 
-    // 5. Create the view file
     await File(viewFilePath).writeAsString(viewContent);
     'Stateful View file created successfully:'.printBoldGreen();
     viewFilePath.printClickablePath();
 
-    // 6. Update router paths and routes after the view file is created successfully
     await updateRouterPaths(featureName, viewName);
     await updateRouterRoutes(featureName, viewName);
   } catch (e) {
-    // Catch any errors that happen during the process
     'An error occurred: $e'.printBoldRed();
   }
 }
@@ -84,10 +78,9 @@ Future<bool> doesRouteExist(String featureName, String viewName) async {
   // Check if the route file exists
   if (!await routesFile.exists()) {
     'Error: Routes file not found.'.printBoldRed();
-    return false; // Abort if the routes file doesn't exist
+    return false;
   }
 
-  // Read the routes file to check if the route for the view already exists
   final routesContent = await routesFile.readAsString();
   final routePattern =
       "${viewName.toPascalCase()}View"; // Match by View class name
