8c01da235e32c36fbe4a2ea21f97f2b3106c9679
Muhammad Umer Malik <77432523+imumermalik@users.noreply.github.com>
2024-10-07 00:31:38 +0500

    Model File path updated

--- lib/features/data_functions/models/functions/create_model_function.dart
+++ lib/features/data_functions/models/functions/create_model_function.dart
@@ -3,39 +3,43 @@ import 'dart:io';
 import 'package:artisan/extensions/clickable_string_console_path.dart';
 import 'package:artisan/extensions/color_print_extension.dart';
 import 'package:artisan/extensions/naming_conventions_extension.dart';
-import 'package:artisan/features/create_feature.dart';
 import 'package:artisan/features/data_functions/models/files/create_model_file.dart';
-
 Future<void> createModel(String modelName, String featureName) async {
   try {
     final projectPath = Directory.current.path;
 
-    final featureDirectory =
-        Directory('$projectPath/lib/features/$featureName/data/models/${modelName.toSnakeCase()}');
+    // Define the full directory path where the model will be created
+    final featureDirectory = Directory('$projectPath/lib/features/$featureName/data/models');
 
+    // Check if the directory exists; if not, create it (recursively)
     if (!await featureDirectory.exists()) {
-      createFeature(featureName);
-      'Feature not found, creating feature: $featureName'.printBoldOrange();
+      featureDirectory.createSync(recursive: true);
+      'Feature directory not found, creating directory: $featureDirectory'.printBoldOrange();
     }
 
-    final modelFilePath =
-        '${featureDirectory.path}/rest_${modelName.toSnakeCase()}_model.dart';
+    // Define the model file path
+    final modelFilePath = '${featureDirectory.path}/rest_${modelName.toSnakeCase()}_model.dart';
 
+    // Check if the file already exists to prevent overwriting
     if (await File(modelFilePath).exists()) {
-      'Error: Model File already exists: '.printBoldRed();
+      'Error: Model file already exists: '.printBoldRed();
       modelFilePath.printClickablePath();
       return;
     }
 
+    // Create the content for the model file
     var contentFile = createModelFile(modelName, featureName);
+
+    // Write the content to the file
     File(modelFilePath).writeAsStringSync(contentFile);
 
     'Model file created successfully: '.printBoldGreen();
-    modelFilePath.toSnakeCase().printClickablePath();
+    modelFilePath.printClickablePath();
   } catch (e) {
+    // Handle different types of errors
     switch (e.runtimeType) {
       case FileSystemException:
-        'Error: Unable to create or write to the entity file.'.printRed();
+        'Error: Unable to create or write to the model file.'.printRed();
         break;
       default:
         'An unknown error occurred: $e'.printBoldRed();
