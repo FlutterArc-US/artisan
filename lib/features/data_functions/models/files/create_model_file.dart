74723bde70e2eaaf4a75df260c87e89e3b6bcbf4
Muhammad Umer Malik <77432523+imumermalik@users.noreply.github.com>
2024-10-06 23:34:01 +0500

    Model File Updatedpath impoetan

--- lib/features/data_functions/models/files/create_model_file.dart
+++ lib/features/data_functions/models/files/create_model_file.dart
@@ -3,7 +3,7 @@ import 'package:artisan/extensions/naming_conventions_extension.dart';
 String createModelFile(String modelName, String featureName) {
   final modelContent = '''
 import 'package:freezed_annotation/freezed_annotation.dart';
-import '../features/${featureName.toSnakeCase()}/domain/models/${modelName.toSnakeCase()}/${modelName.toSnakeCase()}_entity.dart';
+import '../../../${featureName.toSnakeCase()}/domain/models/${modelName.toSnakeCase()}_entity.dart';
 
 part 'rest_${modelName.toSnakeCase()}_entity.freezed.dart';
 part 'rest_${modelName.toSnakeCase()}_entity.g.dart';
@@ -11,8 +11,8 @@ part 'rest_${modelName.toSnakeCase()}_entity.g.dart';
 @freezed
 class Rest${modelName.toPascalCase()}Entity extends ${modelName.toPascalCase()}Entity with _\$Rest${modelName.toPascalCase()}Entity {
   const factory Rest${modelName.toPascalCase()}Entity({
-    required String name,
     required int id,
+    required String name,
   }) = _Rest${modelName.toPascalCase()}Entity;
 
   factory RestAddressEntity.fromJson(Map<String, Object?> json) =>
