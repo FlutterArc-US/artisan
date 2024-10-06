cc05d0f8deb2af11159bb47e4907e986110e5e44
Muhammad Umer Malik <77432523+imumermalik@users.noreply.github.com>
2024-10-06 22:57:27 +0500

    Model Artisan Added

---
+++ lib/functions/features_functions/data_functions/models/files/create_model_file.dart
@@ -0,0 +1,23 @@
+import 'package:artisan/extensions/naming_conventions_extension.dart';
+
+String createModelFile(String modelName, String featureName) {
+  final modelContent = '''
+import 'package:freezed_annotation/freezed_annotation.dart';
+import '../features/${featureName.toSnakeCase()}/domain/models/${modelName.toSnakeCase()}/${modelName.toSnakeCase()}_entity.dart';
+
+part 'rest_${modelName.toSnakeCase}_entity.freezed.dart';
+part 'rest_${modelName.toSnakeCase}_entity.g.dart';
+
+@freezed
+class Rest${modelName.toPascalCase()}Entity extends ${modelName.toPascalCase()}Entity with _\$Rest${modelName.toPascalCase()}Entity {
+  const factory Rest${modelName.toPascalCase()}Entity({
+    required String name,
+    required int id,
+  }) = _Rest${modelName.toPascalCase()}Entity;
+
+  factory RestAddressEntity.fromJson(Map<String, Object?> json) =>
+      _\$Rest${modelName.toPascalCase()}EntityFromJson(json);
+}
+  ''';
+  return modelContent;
+}
---
+++ lib/functions/features_functions/data_functions/models/functions/create_model_function.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:artisan/extensions/clickable_string_console_path.dart';
+import 'package:artisan/extensions/color_print_extension.dart';
+import 'package:artisan/extensions/naming_conventions_extension.dart';
+import 'package:artisan/functions/features_functions/create_feature.dart';
+import 'package:artisan/functions/features_functions/domain_functions/entities/files/create_entity_file.dart';
+
+Future<void> createModel(String modelName, String featureName) async {
+  try {
+    final projectPath = Directory.current.path;
+
+    final featureDirectory =
+    Directory('$projectPath/lib/features/$featureName/data/models');
+
+    if (!await featureDirectory.exists()) {
+      createFeature(featureName);
+      'Feature not found, creating feature: $featureName'.printBoldOrange();
+    }
+
+    final modelFilePath =
+        '${featureDirectory.path}/${modelName.toSnakeCase()}_model.dart';
+
+    if (await File(modelFilePath).exists()) {
+      'Error: Model File already exists: '.printBoldRed();
+      modelFilePath.printClickablePath();
+      return;
+    }
+
+    var contentFile = createEntityFile(modelName);
+    File(modelFilePath).writeAsStringSync(contentFile);
+
+    'Entity file created successfully: '.printBoldGreen();
+    modelFilePath.toSnakeCase().printClickablePath();
+  } catch (e) {
+    switch (e.runtimeType) {
+      case FileSystemException:
+        'Error: Unable to create or write to the entity file.'.printRed();
+        break;
+      default:
+        'An unknown error occurred: $e'.printBoldRed();
+    }
+  }
+}
