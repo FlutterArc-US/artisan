46ca4ae20ce1da55300cbd3ef6e7f40774316f3f
Muhammad Umer Malik <77432523+imumermalik@users.noreply.github.com>
2024-10-07 10:43:45 +0500

    Usecase Settings in progress

--- lib/files/usecase_file.dart
+++ lib/files/usecase_file.dart
@@ -5,7 +5,7 @@ import 'package:artisan/make/make.dart';
 
 //////*********************** Usecase File ***********************//////
 String usecaseFile(String fileName, String featureName) {
-  final className = convertToPascalCase(fileName);
+  final className = fileName.toPascalCase;
   final file = '''
 import 'package:injectable/injectable.dart';
 
@@ -13,7 +13,7 @@ import '../../../../infrastructure/usecase.dart';
 import '../../../../infrastructure/usecase_input.dart';
 import '../../../../infrastructure/usecase_output.dart';
 
-import '../repository/${featureName}_repository.dart';
+import '../repository/${featureName.toSnakeCase()}_repository.dart';
 
 class ${className}UsecaseInput extends Input {
   ${className}UsecaseInput();
@@ -25,15 +25,15 @@ class ${className}UsecaseOutput extends Output {
 
 @lazySingleton
 class ${className}Usecase extends Usecase<${className}UsecaseInput, ${className}UsecaseOutput> {
-  final ${featureName.toPascalCase}Repository _${convertToCamelCase(featureName)}Repository;
+  final ${featureName.toPascalCase()}Repository _${featureName.toCamelCase()}Repository;
 
   ${className}Usecase({required ${convertToPascalCase(featureName)}Repository ${convertToCamelCase(featureName)}Repository})
-      : _${convertToCamelCase(featureName)}Repository = ${convertToCamelCase(featureName)}Repository;
+      : _${featureName.toCamelCase()}Repository = ${featureName.toCamelCase()}Repository;
 
   @override
   Future<${className}UsecaseOutput> call(
       ${className}UsecaseInput input) async {
-        return await _${convertToCamelCase(featureName)}Repository.${convertToCamelCase(fileName.replaceAll('_usecase', ''))}(input);
+        return await _${featureName.toCamelCase()}Repository.${(fileName.toCamelCase().replaceAll('_usecase', ''))}(input);
      }
 }
 ''';
